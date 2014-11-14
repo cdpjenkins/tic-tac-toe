4b5542bb1e508daeea36df0a5637762c842ef513
Chris Jenkins <cdpjenkins@gmail.com>
2014-11-14 16:01:39 +0000

    Detect winning game states

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,10 +1,6 @@
 (ns tic-tac-toe.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
 
 (def blank-board
   [\- \- \-
@@ -18,6 +14,20 @@
    \- \o \-
    \- \- \-])
 
+(def board-x-wins
+  [\x \- \o
+   \- \x \o
+   \- \- \x])
+
+(def board-o-wins
+  [\x \- \o
+   \- \x \o
+   \- \- \o])
+
+(def board-draw
+  [\x \o \o
+   \o \x \x
+   \x \o \o])
 
 (def poses (range 9))
 
@@ -31,7 +41,42 @@
   (doseq [row (partition 3 board)]
     (println (apply str row))))
 
+(defn zip [rest]
+  (apply map vector rest))
+
+(defn winning-partitions [board]
+  (let [rows (partition 3 board)
+        cols (zip rows)
+        diagonals [(apply concat (partition 1 4 board))
+                   (->> board
+                        (drop 2)
+                        (partition 1 2)
+                        (take 3)
+                        (apply concat))]]
+    (concat rows cols diagonals)))
+
+;; 
+;; (apply concat (partition 1 4 (apply concat (map reverse (partition 3 (range 9))))))
+
 (defn possible-moves [board player]
   (doseq [pos (available-squares board)]
     (println)
     (print-board (place-piece board player pos))))
+
+(defn player-has-won [player partitions]
+  (some (fn [partition] (every? #(= player %) partition)) partitions))
+
+(defn winner
+  "Return one of x, o, nil, :draw"
+  ([board]
+     (let [partitions (winning-partitions board)]
+       (cond
+        (player-has-won \x partitions) \x
+        (player-has-won \o partitions) \o
+        (empty? (available-squares board)) :draw
+        :else nil))))
+
+
+;;0 1 2
+;;3 4 5
+;;6 7 8
